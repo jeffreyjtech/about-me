3792156ea1fc2fbc0dc6aab5d27c142326dea07f
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-01-11 14:02:01 -0800

    Added first question prompt and response

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-let user = prompt('What is your name?');
+let user = prompt('Welcome to the site! What is your name?');
 
-alert(`Welcome ${user}`);
+alert(`Welcome ${user}, I'm Jeff, the owner of this website.\nThis is a trivia game about me so that you might get to know me. Enjoy!`);
 
+let question = 'Have I ever traveled outside of the US?';
+let correctanswer = 'yes';
+let response = prompt(question).toLowerCase();
+if (correctanswer === response){
+  alert('Correct! I have traveled outside the US!');
+} else if ('string' === typeof response) {
+  alert('Oops, wrong answer.');
+} else {
+  alert('That wasn\'t even a word... \nMoving on.');
+}
