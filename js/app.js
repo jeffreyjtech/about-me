c066d0b55141b837f4a0a7ff3f11977a65961adb
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-01-12 22:43:08 -0800

    Number guessing game now reveals the magic number on pass or fail

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const qarArray = [
   ['Have I ever walked on the moon?','no','Nope. I\'d pay any sum of money to walk on the Moon.']
 ];
 
-console.table(qarArray);
+//console.table(qarArray);
 
 let correctcount = 0;
 
@@ -38,26 +38,33 @@ for (let i = qarArray.length-1; i >= 0; i--){
 
 // Number guessing game
 
-// This creates the "magic number".
+// This randomly generates the "magic number".
 // Math.random() returns a random decimal between 0 and 1
 // I then multiply by 9 and add 1. The resulting decimal number should always be between 1 and 10.
 // Then Math.round() rounds the decimal number to the nearest integer
 
 let magicNumber = Math.round(Math.random()*9+1);
-console.log(magicNumber);
+//console.log(magicNumber);
 
-for (let i = 0; i < 4; i++){
-  let guessnum = parseInt(prompt('Guess the magic number! It\'s between 1 and 10. You have 4 tries.'));
+for (let i = 4; i > 0; i--){
+  let correctGuess = false;
+
+  let guessnum = parseInt(prompt(`Guess the magic number! It's a whole number between 1 and 10. You have ${i} tries.`));
   if (guessnum === magicNumber){
-    i = 3;
+    i = 1;
     alert(`You got it! The magic number is ${magicNumber}.`);
     correctcount++;
+    correctGuess = true;
   } else if (guessnum > magicNumber){
-    alert('Nope! Try a lower number.');
+    alert('Nope! That\'s too high.');
   } else if (guessnum < magicNumber){
-    alert('Nope! Try a higher number.');
+    alert('Nope! That\'s too low.');
   } else {
-    alert('Nope! That wasn\'t a valid number');
+    alert('Nope! That wasn\'t a valid number.');
+  }
+
+  if(!correctGuess && i === 1){
+    alert(`You've run out of tries. The magic number is ${magicNumber}.`);
   }
 }
 
@@ -72,6 +79,7 @@ for (let tries = 6; tries > 0; tries--){
   //console.log(answer);
 
   // this boolean variable needs to exist to silence the failure prompts if the user's answer was correct
+  // I believe I could avoid this with a break statement
   let correctGuess = false;
 
   // this for loop will check the answer against each string in the array
@@ -94,72 +102,3 @@ for (let tries = 6; tries > 0; tries--){
 }
 
 alert(`Thanks for playing ${user}. You got ${correctcount} out of 7 questions correct.`);
-
-// This is the previous code for the personal trivia. I made a format that I could easily copy and paste. I would have used a function if it wasn't out of scope of the material.
-
-/*
-let question = 'Have I ever traveled outside of the US?','yes';
-let correctanswer = 'yes';
-let response = prompt(question);
-if (correctanswer === response.toLowerCase()){
-  correctcount++;
-  alert('Correct! I have traveled outside the US!');
-  //console.log(`Correct on "${question}"`);
-} else if ('string' === typeof response) {
-  alert('Sorry, wrong answer.');
-} else {
-  alert('That wasn\'t even a word... \nMoving on.');
-}
-
-question = 'Do I hate learning?';
-correctanswer = 'no';
-response = prompt(question);
-if (correctanswer === response.toLowerCase()){
-  correctcount++;
-  alert('Correct! I don\'t hate learning, I love it!');
-  //console.log(`Correct on "${question}"`);
-} else if ('string' === typeof response) {
-  alert('Sorry, wrong answer.');
-} else {
-  alert('That wasn\'t even a word... \nMoving on.');
-}
-
-question = 'Can I bake bread?';
-correctanswer = 'yes';
-response = prompt(question);
-if (correctanswer === response.toLowerCase()){
-  correctcount++;
-  alert('Correct! I make delicious bread.');
-  //console.log(`Correct on "${question}"`);
-} else if ('string' === typeof response) {
-  alert('Sorry, wrong answer.');
-} else {
-  alert('That wasn\'t even a word... \nMoving on.');
-}
-
-question = 'Do I enjoy chili peppers?';
-correctanswer = 'no';
-response = prompt(question);
-if (correctanswer === response.toLowerCase()){
-  correctcount++;
-  alert('Correct! I hate chili peppers and spicy food in general.');
-  //console.log(`Correct on "${question}"`);
-} else if ('string' === typeof response) {
-  alert('Sorry, wrong answer.');
-} else {
-  alert('That wasn\'t even a word... \nMoving on.');
-}
-
-question = 'Have I ever walked on the moon?';
-correctanswer = 'no';
-response = prompt(question);
-if (correctanswer === response.toLowerCase()){
-  correctcount++;
-  alert('I\'d pay any sum of money to walk on the Moon.');
-  //console.log(`Correct on "${question}"`);
-} else if ('string' === typeof response) {
-  alert('Regrettably, I have not walked on the moon and your answer is right out.');
-} else {
-  alert('That wasn\'t even a word... \nMoving on.');
-}
-*/
